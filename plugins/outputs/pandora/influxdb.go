d7cb805be86ea0d3339604e965cdcaf1c1c75a22
mengjinglei <mengjinglei@qiniu.com>
2017-03-15 17:00:32 +0800

    Fix

---
+++ plugins/outputs/pandora/influxdb.go
@@ -0,0 +1,136 @@
+package pandora
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/internal"
+	"github.com/influxdata/telegraf/metric"
+	"github.com/influxdata/telegraf/plugins/outputs"
+
+	"qiniu.com/pandora/pipeline"
+	"qiniu.com/pandora/tsdb"
+
+	sdkbase "qiniu.com/pandora/base"
+)
+
+type PandoraTSDB struct {
+	// URL is only for backwards compatability
+	URL              string            `toml:"url"`
+	AK               string            `toml:"ak"`
+	SK               string            `toml:"sk"`
+	Repo             string            `toml:"repo"`
+	RetentionPolicy  string            `toml:"retention_policy`
+	AutoCreateSeries bool              `toml:"auto_create_series`
+	Timeout          internal.Duration `toml:"timeout"`
+
+	client tsdb.TsdbAPI
+}
+
+var sampleConfig = `
+ # Configuration for PandoraTSDB server to send metrics to
+[[outputs.PandoraTSDB]]
+  url = "http://localhost:8086" # required
+  ## The target repo for metrics (telegraf will create it if not exists).
+  repo = "telegraf" # required
+  
+  ## 是否自动创建series
+  auto_create_series = false
+  ## 自创创建的series的retention，支持的retention为[1-30]d
+  retention_policy = ""
+  ## Write timeout (for the PandoraTSDB client), formatted as a string.
+  ## If not provided, will default to 5s. 0s means no timeout (not recommended).
+  timeout = "5s"
+  ak = "ACCESS_KEY"
+  sk = "SECRET_KEY"
+`
+
+func (i *PandoraTSDB) Connect() error {
+
+	u, err := url.Parse(i.URL)
+	if err != nil {
+		return fmt.Errorf("error parsing config.URL: %s", err)
+	}
+	if u.Scheme != "http" && u.Scheme != "https" {
+		return fmt.Errorf("config.URL scheme must be http(s), got %s", u.Scheme)
+	}
+	log.Println(u.String())
+	cfg := pipeline.NewConfig().
+		WithAccessKeySecretKey(i.AK, i.SK).
+		WithEndpoint(i.URL).
+		WithLogger(sdkbase.NewDefaultLogger()).
+		WithLoggerLevel(sdkbase.LogDebug).
+		WithResponseTimeout(i.Timeout.Duration)
+
+	// 生成client实例
+	client, err := tsdb.New(cfg)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	i.client = client
+
+	return nil
+}
+
+func (i *PandoraTSDB) Close() error {
+	return nil
+}
+
+func (i *PandoraTSDB) SampleConfig() string {
+	return sampleConfig
+}
+
+func (i *PandoraTSDB) Description() string {
+	return "Configuration for PandoraTSDB server to send metrics to"
+}
+
+// Choose a random server in the cluster to write to until a successful write
+// occurs, logging each unsuccessful. If all servers fail, return error.
+func (i *PandoraTSDB) Write(metrics []telegraf.Metric) error {
+	bufsize := 0
+	for _, m := range metrics {
+		bufsize += m.Len()
+	}
+	r := metric.NewReader(metrics)
+	p := make([]byte, bufsize)
+	n, err := r.Read(p)
+	if err != nil {
+		return err
+	}
+	log.Println(bufsize, n, string(p))
+	// This will get set to nil if a successful write occurs
+	err = fmt.Errorf("Could not write to any PandoraTSDB server in cluster")
+
+	if e := i.client.PostPointsFromBytes(&tsdb.PostPointsFromBytesInput{
+		RepoName: i.Repo,
+		Buffer:   p,
+	}); e != nil {
+		if strings.Contains(e.Error(), "field type conflict") {
+			log.Printf("E! Field type conflict, dropping conflicted points: %s", e)
+			// setting err to nil, otherwise we will keep retrying and points
+			// w/ conflicting types will get stuck in the buffer forever.
+			err = nil
+		}
+		// Log write failure
+		log.Printf("E! PandoraTSDB Output Error: %s", e)
+	} else {
+		err = nil
+	}
+
+	return err
+}
+
+func newPandoraTSDB() *PandoraTSDB {
+	return &PandoraTSDB{
+		Timeout: internal.Duration{Duration: time.Second * 5},
+	}
+}
+
+func init() {
+	outputs.Add("PandoraTSDB", func() telegraf.Output { return newPandoraTSDB() })
+}
